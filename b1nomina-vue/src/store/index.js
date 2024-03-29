cc93f764f95484eecb8a5b5037164054d30e367d
Mastrant <ortegagregorio986@gmail.com>
2024-03-29 08:46:34 -0400

    fix

--- b1nomina-vue/src/store/index.js
+++ b1nomina-vue/src/store/index.js
@@ -1,5 +1,5 @@
 import { createStore, } from "vuex";
-import { perfilModulo } from './modulos/perfil-vuex'
+import  perfilModulo  from './modulos/perfil-vuex'
 
 const store = createStore({    
     modules:{
