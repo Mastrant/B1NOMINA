edc10fd899d7befbd7a149d01869d9b6ba48fde9
GAOP <ortegagregorio986@gmail.com>
2024-05-19 23:37:25 -0400

    fix

--- b1nomina-vue/src/peticiones/configuracion/datos_empresa.js
+++ b1nomina-vue/src/peticiones/configuracion/datos_empresa.js
@@ -159,6 +159,23 @@ const peticiones_configuracion_datosEmpresa = {
             return { success: false, error: error };
         }  
     }, 
+    getDatosPrevisionalesBasicos(sociedadID){
+        try {
+            // Realiza una petición GET a la API para obtener los datos del representante de la empresa.
+            return axios.get(`sociedad/${sociedadID}/get_campos_adicionales`)
+            // Si la petición es exitosa, devuelve un objeto con éxito y los datos recibidos.
+           .then(respuesta => {
+                return { success: true, data: respuesta?.data };
+            })
+            // En caso de error en la petición, devuelve un objeto indicando el fallo y el mensaje de error.
+           .catch(error => {
+                return { success: false, error: error?.response };
+            });
+        } catch (error) {
+            // Captura errores generales del bloque try-catch y devuelve un objeto indicando el fallo.
+            return { success: false, error: error };
+        }  
+    }, 
 
 }
 
