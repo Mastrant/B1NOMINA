21f9ba7ed05a47b216b5489ffdfce20ecc8cf9d5
GAOP <ortegagregorio986@gmail.com>
2024-05-30 19:59:16 -0400

    creado nuevo componente de tipo input

--- b1nomina-vue/src/components/panel/Panel-Dashboard.vue
+++ b1nomina-vue/src/components/panel/Panel-Dashboard.vue
@@ -1,13 +1,26 @@
 <template>
     <div class="conted">
         <span>En Desarrollo, gracias por su paciencia.</span>
-        
+        <InputBorderList
+                Placeholder="Ingresar número de cuenta"
+                Titulo="Cuenta bonos feriado"
+                name="CuentaBonosFeriados"
+                v-model="variable"
+                @update:modelValue="variable = $event"
+                :requerido="false"
+                :opciones="[{id:1, nombre:'predro'}]"
+            />
     </div>
 </template>
 
 <script setup>
+import InputBorderList from '@/components/inputs/Input-Border-List.vue';
+import { ref, watch } from 'vue';
+const variable = ref('')
 
-
+watch(variable, (newv) => {
+    console.log(newv)
+})
 </script>
 
 <style scoped>
